c4a8bfba8c66e08d6dafae014f02414930b417ba
Sm Angina <bdbakka33@gmail.com>
2025-01-30 15:49:59 +0600

    ui improved & added more

--- functional.js
+++ functional.js
@@ -1,56 +1,51 @@
-let next = document.querySelector('.next-btn')
-let favBtn = document.querySelector('.fav-btn')
+let next = document.querySelector(".next-btn");
+let favBtn = document.querySelector(".fav-btn");
 let i = 0;
 let speed = 30;
-let qIndex = Math.floor(Math.random()*quotes.length)
+let qIndex = Math.floor(Math.random() * quotes.length);
 var crntQuote = quotes[qIndex];
+
 function writeQuote() {
-let author = document.querySelector('.qtnist')
-author.innerHTML = `<img src="data:image/${crntQuote.qA}">`
+  let author = document.querySelector(".qtnist");
+  author.innerHTML = `<img src="data:image/${crntQuote.qA}">`;
   if (i < crntQuote.q.length) {
     document.getElementById("quote").innerHTML += crntQuote.q.charAt(i);
     i++;
     setTimeout(writeQuote, speed);
   }
 }
-writeQuote()
+writeQuote();
 
-next.addEventListener('click', writeNext)
-function writeNext(){
-  let j = Math.floor((Math.random())*quotes.length)
-    qIndex = j;
-    crntQuote = quotes[qIndex]
-    document.getElementById("quote").innerHTML = "";
-    i = 0;
-    writeQuote()
-    console.log(qIndex)
-    if(crntQuote.isFav == 'no'){
-      favBtn.querySelector('i').style.color = 'white'
-    }else{
-      favBtn.querySelector('i').style.color = 'var(--text)'
-    
-    }
-
+next.addEventListener("click", writeNext);
+function writeNext() {
+  let j = Math.floor(Math.random() * quotes.length);
+  qIndex = j;
+  crntQuote = quotes[qIndex];
+  document.getElementById("quote").innerHTML = "";
+  i = 0;
+  writeQuote();
+  if (crntQuote.isFav == "no") {
+    favBtn.querySelector("i").style.color = "white";
+  } else {
+    favBtn.querySelector("i").style.color = "var(--text)";
+  }
 }
 
-favBtn.addEventListener('click', faved)
-function faved(){
-  if(localStorage.getItem(`q${qIndex}isFav`) == 'yes'){
-    localStorage.setItem(`q${qIndex}isFav`, 'no')
-    favBtn.querySelector('i').style.color = '#fff'
-    crntQuote.isFav = 'no'
-  }else{
-      localStorage.setItem(`q${qIndex}isFav`, 'yes')
-      favBtn.querySelector('i').style.color = 'var(--text)'
-    crntQuote.isFav = 'yes'
-
-    }
+favBtn.addEventListener("click", faved);
+function faved() {
+  if (localStorage.getItem(`q${qIndex}isFav`) == "yes") {
+    localStorage.setItem(`q${qIndex}isFav`, "no");
+    favBtn.querySelector("i").style.color = "#fff";
+    crntQuote.isFav = "no";
+  } else {
+    localStorage.setItem(`q${qIndex}isFav`, "yes");
+    favBtn.querySelector("i").style.color = "var(--text)";
+    crntQuote.isFav = "yes";
+  }
 }
 
-
-if(crntQuote.isFav == 'yes'){
-  favBtn.querySelector('i').style.color = 'var(--text)'
-}else{
-  favBtn.querySelector('i').style.color = 'white'
-
+if (crntQuote.isFav == "yes") {
+  favBtn.querySelector("i").style.color = "var(--text)";
+} else {
+  favBtn.querySelector("i").style.color = "white";
 }
